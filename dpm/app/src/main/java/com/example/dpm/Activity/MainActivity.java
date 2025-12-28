b7c9f3f9b2bd91056e0ac276d38ead5ac23459cf
Petar Dragicevic <dragicevicpetar14@gmail.com>
2025-12-28 18:06:34 +0100

    [ADD] Driver history gui

--- dpm/app/src/main/java/com/example/dpm/Activity/MainActivity.java
+++ dpm/app/src/main/java/com/example/dpm/Activity/MainActivity.java
@@ -1,19 +1,13 @@
-package com.example.dpm;
+package com.example.dpm.Activity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.dpm.Fragment.DriveHistoryFragment;
+import com.example.dpm.R;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -21,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     MaterialToolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawerLayout);
         toolbar = findViewById(R.id.materialToolbar);
+        navigationView = findViewById(R.id.navigationView);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+
         toolbar.setNavigationOnClickListener(v ->
                 drawerLayout.openDrawer(GravityCompat.START)
         );
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.nav_history) {
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace( R.id.frameLayout, new DriveHistoryFragment())
+                        .commit();
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+
+            return false;
+        });
     }
 
 
